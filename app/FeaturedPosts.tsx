87e94dde1c7173c645d792540a6ba9d183529dea
MD Pabel <mdpabel385@gmail.com>
2023-06-01 21:46:23 +0600

    Added hero compoent

--- app/FeaturedPosts.tsx
+++ app/FeaturedPosts.tsx
@@ -1,23 +1,21 @@
-import { BigCard, SmallCard } from '@/components/Card'
-import ComponentWrapper from '@/components/ComponentWrapper'
-import React from 'react'
+import { BigCard, SmallCard } from '@/components/Card';
+import ComponentWrapper from '@/components/ComponentWrapper';
+import React from 'react';
 
 const FeaturedPosts = () => {
-    return (
+  return (
+    <ComponentWrapper className='flex space-x-8'>
+      <div className='w-1/3'>
+        <BigCard className='w-full' />
+      </div>
 
-        <ComponentWrapper className="flex space-x-8">
-            <div className='w-1/3'>
-                <BigCard className='w-full' />
-            </div>
+      <div className='w-1/3 space-y-4'>
+        <SmallCard />
+        <SmallCard />
+        <SmallCard />
+      </div>
+    </ComponentWrapper>
+  );
+};
 
-            <div className='w-1/3 space-y-4'>
-                <SmallCard />
-                <SmallCard />
-                <SmallCard />
-            </div>
-        </ComponentWrapper >
-
-    )
-}
-
-export default FeaturedPosts
+export default FeaturedPosts;
--- app/Hero.tsx
+++ app/Hero.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
-    return (
-        <section className='container mx-auto'>
-            <h1>Unmasking Threats</h1>
-            <h2>Hackers don't take vacations. Keep your WordPress site secure and give them no chance to exploit vulnerabilities.</h2>
-        </section>
-    )
-}
+  return (
+    <section className='container flex flex-col items-center justify-center px-4 mx-auto space-y-5 md:px-0 h-52 md:h-60'>
+      <h1 className='text-2xl font-semibold text-center md:text-6xl'>
+        Unmasking Threats
+      </h1>
+      <h2 className='mx-auto text-base font-medium text-center md:text-lg'>
+        Hackers don't take vacations. Keep your WordPress site secure and give
+        them no chance to exploit vulnerabilities.
+      </h2>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
