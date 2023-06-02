349f42928b504ba19c749207cbd75d1b3765c1dc
MD Pabel <mdpabel385@gmail.com>
2023-06-02 15:08:24 +0600

    adjust with mobile device

---
+++ app/Newsletter.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Newsletter = () => {
+  return (
+    <div>
+      <h2>Get more updates...</h2>
+      <p>
+        Do you want to get notified when a new component is added to Flowbite?
+        Sign up for our newsletter and you&apos;ll be among the first to find
+        out about new features, components, versions, and tools.
+      </p>
+
+      <form>
+        <input type='email' name='' id='' />
+        <button type='submit'>Subscribe</button>
+      </form>
+
+      <p>
+        By subscribing, you agree with wordpresssecurities&apos;s Terms of
+        Service and Privacy Policy.
+      </p>
+    </div>
+  );
+};
+
+export default Newsletter;
