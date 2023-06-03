3e3fc68a711258eae05127e2749aa1dd7417089c
MD Pabel <mdpabel385@gmail.com>
2023-06-03 23:30:00 +0600

    Designed featured coupon component

--- app/FeaturedCoupons.tsx
+++ app/FeaturedCoupons.tsx
@@ -7,8 +7,7 @@ const FeaturedCoupons = () => {
   return (
     <ComponentWrapper className='pt-10'>
       <Title>WordPress best deals & coupons</Title>
-      <div className='grid grid-cols-1 gap-8 pt-10 md:grid-cols-2 lg:grid-cols-4'>
-        <HostingCard />
+      <div className='grid grid-cols-1 gap-8 pt-10 md:grid-cols-2 lg:grid-cols-3'>
         <HostingCard />
         <HostingCard />
         <HostingCard />
--- app/Newsletter.tsx
+++ app/Newsletter.tsx
@@ -25,7 +25,7 @@ const Newsletter = () => {
           <input
             type='text'
             id='email-address-icon'
-            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 '
+            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 '
             placeholder='contact@wordpresssecurities.com'
           />
         </div>
