9abc42cd95c67026585f446589fce8a5a7bb8a04
MD Pabel <mdpabel385@gmail.com>
2023-06-08 11:16:21 +0600

    before remove tiptap editor modules

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 import ComponentWrapper from '@/components/ComponentWrapper';
 import React from 'react';
-import TextEditor from './Tiptap';
 import Title from './Title';
 import EditorJodit from './EditorJodit';
 
@@ -8,7 +7,6 @@ const Dashboard = () => {
   return (
     <ComponentWrapper className='pt-10 space-y-5'>
       <Title />
-      {/* <TextEditor /> */}
       <EditorJodit />
     </ComponentWrapper>
   );
