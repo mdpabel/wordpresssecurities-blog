36c067937bfb7211d34780e76d40749fcd620a41
unknown <mdpabel385@gmail.com>
2023-07-07 02:47:36 +0600

    allowed img tag in sanitizeHtml configuration

--- app/(AuthenticatedApp)/ai/page.tsx
+++ app/(AuthenticatedApp)/ai/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import withAuth from '@/components/common/withAuth';
 import React from 'react';
 
--- app/(AuthenticatedApp)/profile/page.tsx
+++ app/(AuthenticatedApp)/profile/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import ComponentWrapper from '@/components/common/ComponentWrapper';
 import withAuth from '@/components/common/withAuth';
 import { UserProfile } from '@clerk/nextjs';
